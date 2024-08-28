705ebd5206201fb6e78680b6bf033fcbe752d29e
PhilippKDT <p.kleidt@mbd-team.de>
2024-08-28 14:36:54 +0200

    input wird in Konsole wiedergegeben und clear button funktioniert

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,27 @@
 import "./style.css";
+
+const addEL = document.querySelectorAll("section button");
+
+let speicher: number | string = "";
+
+for (let i = 0; i < addEL.length; i++) {
+  let itemOfList = addEL[i].id;
+  (<HTMLButtonElement>document.getElementById(itemOfList)).addEventListener(
+    "click",
+    () => input(itemOfList),
+    true
+  );
+}
+
+function input(InputId: string) {
+  if (InputId === "clear") {
+    speicher = "";
+  } else {
+    speicher += InputId;
+    console.log(speicher);
+  }
+}
+
+// function updateNumber() {
+//   (<HTMLButtonElement>document.getElementById("start")).innerHTML = speicher;
+// }
