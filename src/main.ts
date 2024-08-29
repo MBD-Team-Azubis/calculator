fce56b37a04948b139c715403b86738c1d0e0b9d
PhilippKDT <p.kleidt@mbd-team.de>
2024-08-29 11:31:27 +0200

    Display is working now

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,25 @@
 import "./style.css";
 
-let input = document.getElementById("inputbox");
-let button = document.querySelectorAll("button");
+const button = document.querySelectorAll("section input");
 
-let speicher: "";
-let arr = Array.from(button);
+let speicher = 0;
+let display = "";
+let output = "";
 
-arr.forEach((button) =>
-  button.addEventListener("click", () => inputCheck(), true)
-);
+for (let i = 0; i < button.length; i++) {
+  const itemOfList = button[i].id;
+  const change = parseInt(itemOfList);
 
-function inputCheck() {}
+  (<HTMLInputElement>document.getElementById(itemOfList)).addEventListener(
+    "click",
+    () => inputCheck(change),
+    true
+  );
+}
+
+function inputCheck(InputId: number) {
+  display = display + InputId.toString();
+  output = display;
+  (<HTMLDivElement>document.getElementById("output")).innerHTML = output;
+  console.log(display);
+}
