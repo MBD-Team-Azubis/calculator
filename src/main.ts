c0ca464e270f8a0a58471981dcd8e8434ceb4ae0
PhilippKDT <p.kleidt@mbd-team.de>
2024-08-29 09:34:35 +0200

    new design

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,10 @@ import "./style.css";
 
 const addEL = document.querySelectorAll("section button");
 
-let speicher: number | string = "";
+let speicher = 0;
+let oldspeicher = 0;
+let display = "";
+let result = 0;
 
 for (let i = 0; i < addEL.length; i++) {
   let itemOfList = addEL[i].id;
@@ -11,17 +14,22 @@ for (let i = 0; i < addEL.length; i++) {
     () => input(itemOfList),
     true
   );
+  (<HTMLSpanElement>document.getElementById(itemOfList)).addEventListener(
+    "click",
+    () => updateNumber(itemOfList),
+    true
+  );
 }
 
 function input(InputId: string) {
   if (InputId === "clear") {
-    speicher = "";
+    window.location.reload();
+  } else if (InputId === " + ") {
+    speicher = parseInt(display);
+    result = oldspeicher + speicher;
+    oldspeicher = result;
   } else {
     speicher += InputId;
     console.log(speicher);
   }
 }
-
-// function updateNumber() {
-//   (<HTMLButtonElement>document.getElementById("start")).innerHTML = speicher;
-// }
