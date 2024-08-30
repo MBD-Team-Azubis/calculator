d2745c644c174c57c2d33fb77e5a130b759d9820
PhilippKDT <p.kleidt@mbd-team.de>
2024-08-30 11:59:42 +0200

    Operator Konzept funktioniert von den Variablen her. Function für das Basteln eines greatOutputs aus zwei Arrays integriert.

--- src/main.ts
+++ src/main.ts
@@ -1,25 +1,91 @@
 import "./style.css";
 
 const button = document.querySelectorAll("section input");
+const operators = document.querySelectorAll("section button");
 
-let speicher = 0;
 let display = "";
 let output = "";
+let counter = 0;
+let numberSpeicher: number[] = [];
+let operatorSpeicher: string[] = [];
+let speicherStage: number | string = 0;
+let teilzeitOp = "";
+let greatOutput: (number | string)[] = [];
+let finalDisplay = "";
 
 for (let i = 0; i < button.length; i++) {
   const itemOfList = button[i].id;
   const change = parseInt(itemOfList);
-
   (<HTMLInputElement>document.getElementById(itemOfList)).addEventListener(
     "click",
-    () => inputCheck(change),
-    true
+    () => input(change)
   );
 }
 
-function inputCheck(InputId: number) {
+for (let i = 0; i < operators.length; i++) {
+  const operatorList = operators[i].id;
+  (<HTMLButtonElement>document.getElementById(operatorList)).addEventListener(
+    "click",
+    () => operatorUse(operatorList)
+  );
+}
+
+// sorgt für den Input im display.
+function input(InputId: number) {
   display = display + InputId.toString();
   output = display;
+
   (<HTMLDivElement>document.getElementById("output")).innerHTML = output;
-  console.log(display);
+}
+
+function newOutput() {
+  for (let h = 0; h < numberSpeicher.length; h++) {
+    greatOutput.push(numberSpeicher[h]);
+    for (let k = 0; k < operatorSpeicher.length; k++) {
+      greatOutput.push(operatorSpeicher[k]);
+    }
+  }
+  finalDisplay = finalDisplay + greatOutput.toString();
+}
+
+function operatorUse(opInput: string) {
+  if (opInput === "clear") {
+    (<HTMLDivElement>document.getElementById("output")).innerHTML = output;
+    console.log(output);
+  } else if (opInput === "-") {
+    speicherStage = parseInt(output);
+    numberSpeicher[counter] = speicherStage;
+    speicherStage = 0;
+    teilzeitOp = "-";
+    operatorSpeicher[counter] = teilzeitOp;
+    teilzeitOp = "";
+    counter++;
+  } else if (opInput === "+") {
+    speicherStage = parseInt(output);
+    numberSpeicher[counter] = speicherStage;
+    speicherStage = 0;
+    teilzeitOp = "+";
+    operatorSpeicher[counter] = teilzeitOp;
+    teilzeitOp = "";
+    counter++;
+  } else if (opInput === "/") {
+    speicherStage = parseInt(output);
+    numberSpeicher[counter] = speicherStage;
+    speicherStage = 0;
+    teilzeitOp = "/";
+    operatorSpeicher[counter] = teilzeitOp;
+    teilzeitOp = "";
+    counter++;
+  } else if (opInput === "*") {
+    speicherStage = parseInt(output);
+    numberSpeicher[counter] = speicherStage;
+    speicherStage = 0;
+    teilzeitOp = "*";
+    operatorSpeicher[counter] = teilzeitOp;
+    teilzeitOp = "";
+    counter++;
+  } else if (opInput === "=") {
+  } else if (opInput === "delete") {
+  } else {
+  }
 }
