76f4f38fdaa8251e5f4973ae8e8a782f5ef9de17
PhilippKDT <p.kleidt@mbd-team.de>
2024-09-03 11:30:34 +0200

    everything is working "so far"

--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,8 @@
 import "./style.css";
 
 const button = document.querySelectorAll("section input");
-const operators = document.querySelectorAll("section button");
 
 let output = "";
-let numberSpeicher: number[] = [];
-let operatorSpeicher: string[] = [];
-let speicherStage = 0;
-let greatOutput: (number | string)[] = [];
-let finalDisplay = "";
 
 for (let i = 0; i < button.length; i++) {
   const item = button[i].id;
@@ -18,25 +12,16 @@ for (let i = 0; i < button.length; i++) {
   );
 }
 
-for (let d = 0; d < operators.length; d++) {
-  const operator = operators[d].id;
-  (<HTMLButtonElement>document.getElementById(operator)).addEventListener(
-    "click",
-    () => operatorUse(operator)
-  );
-}
-
-// sorgt für den Input im display.
 function input(InputId: string) {
   if (InputId === "=") {
-    (<HTMLDivElement>document.getElementById("output")).innerText =
-      evaluate(output);
+    output = evaluate(output);
+    (<HTMLDivElement>document.getElementById("output")).innerText = output;
   } else if (InputId === "clear") {
     (<HTMLDivElement>document.getElementById("output")).innerText = "0";
+    output = "";
   } else if (InputId === "delete") {
-    output.substring(output.length - 1);
+    output = output.slice(0, -1);
     (<HTMLDivElement>document.getElementById("output")).innerText = output;
-    console.log(output);
   } else {
     output += InputId;
     (<HTMLDivElement>document.getElementById("output")).innerText = output;
@@ -45,47 +30,4 @@ function input(InputId: string) {
 
 function evaluate(fn: string) {
   return new Function("return " + fn)();
-}
-
-function newOutput() {
-  for (let h = 0; h < numberSpeicher.length; h++) {
-    greatOutput.push(numberSpeicher[h]);
-    for (let k = 0; k < operatorSpeicher.length; k++) {
-      greatOutput.push(operatorSpeicher[k]);
-    }
-  }
-
-  finalDisplay = finalDisplay + greatOutput.join("");
-  console.log(finalDisplay);
-  (<HTMLDivElement>document.getElementById("output")).innerText = finalDisplay;
-}
-
-function operatorUse(opInput: string) {
-  if (opInput === "clear") {
-    (<HTMLDivElement>document.getElementById("output")).innerText = "0";
-  } else if (opInput === "-") {
-    speicherStage = parseInt(output);
-    numberSpeicher.push(speicherStage);
-    operatorSpeicher.push(opInput);
-    newOutput();
-  } else if (opInput === "+") {
-    speicherStage = parseInt(output);
-    numberSpeicher.push(speicherStage);
-    operatorSpeicher.push(opInput);
-    newOutput();
-  } else if (opInput === "/") {
-    speicherStage = parseInt(output);
-    numberSpeicher.push(speicherStage);
-    operatorSpeicher.push(opInput);
-    newOutput();
-  } else if (opInput === "*") {
-    speicherStage = parseInt(output);
-    numberSpeicher.push(speicherStage);
-    operatorSpeicher.push(opInput);
-    newOutput();
-  } else if (opInput === "=") {
-  } else if (opInput === "delete") {
-  } else {
-    newOutput();
-  }
 }
